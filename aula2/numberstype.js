ad1b24e135b8931f957ab155889ab161e49b9c82
Igor <igorxt9@gmail.com>
2021-03-23 01:03:21 -0300

    HTML and his JS associated file commit

---
+++ aula2/numberstype.js
@@ -0,0 +1,25 @@
+/*
+    ***Aula 1***
+*/
+console.log(typeof 15);
+console.log(typeof 12.7);
+console.log(typeof -4);
+console.log(typeof "Igor");
+console.log(14%3);
+
+/* 
+    ***Aula Strings***
+*/
+console.log (`A soma \n de 2+2 é: ${2+2}`); /* <~ Templete strings */
+console.log ("Salada " + "de " + "Fruta");
+
+/*
+    ***Aula boolean***
+*/
+console.log(5 != 6); /* | ==: igual; ===: idêntico (mesmo tipo); */
+console.log(5 === "5"); /* Resultado = False, com == o resultado é true; */
+
+/*
+    Operador ternário ("substitui" o if)
+*/
+console.log(2 > 5 ? "Sim" : "Não"); /* Return: Não */
