e504e707fc280cbd1ee67c0982e9818b005fd78f
Igor <igorxt9@gmail.com>
2021-03-23 22:02:01 -0300

    First exercise of the course

---
+++ exercicio1/exercicio1.js
@@ -0,0 +1,23 @@
+/* 1) Escrever 3 valores em string em um arquivo e exibir no navegador */
+    console.log("Hello World!");
+    console.log('Olá mundo!');
+    console.log(`Hola mundo!`); /* template literals */
+
+/* 2) Escrever 3 valores em number e exibir no navegador. Número inteiro, com casa decimal e por aritmética. */
+    console.log(45);
+    console.log(45.2);
+    console.log((45*2)+15);
+
+/* 3) Escreva três comparações com boolean. >, < e != */
+    console.log(5 > 6);
+    console.log(10 <= 10);
+    console.log("Igor" != "Matheus");
+
+/* 4) Escreva três comparações com op lógicos and, or e not. */
+    console.log(5 === 5 && 10 > 2); /* and */
+    console.log(6 >= 5 || "Igor" === "Matheus"); /* or */
+    console.log(!(3010 === 3010)); /* not */
+
+/* Faça uma operação que emita um NaN no console do navegador */
+    console.log(3010 - "Igor");
+    console.log(``);
